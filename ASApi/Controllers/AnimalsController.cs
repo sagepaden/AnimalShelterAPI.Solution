d701b70022125f6db6adb5f932de2cc500c225f3
sagepaden <sagepaden@gmail.com>
2023-08-25 13:01:51 -0700

    finishing touches of initial animalscontroller

--- ASApi/Controllers/AnimalsController.cs
+++ ASApi/Controllers/AnimalsController.cs
@@ -50,5 +50,76 @@ namespace ASApi.Controllers.v1
       }
       return await query.ToListAsync();
     }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Animal>> GetAnimal(int id)
+    {
+      Animal animal = await _db.Animals
+                            .FirstOrDefaultAsync(animal => animal.AnimalId == id);
+      if (animal == null)
+      {
+        return NotFound();
+      }
+      return animal;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Animal>> Post([FromBody] Animal animal)
+    {
+      _db.Animals.Add(animal);
+      await _db.SaveChangesAsync();
+      return CreatedAtAction(nameof(GetAnimal), new { id = animal.AnimalId }, animal);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Animal animal)
+    {
+      if (id != animal.AnimalId)
+      {
+        return BadRequest();
+      }
+
+      _db.Animals.Update(animal);
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!AnimalExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAnimal(int id)
+    {
+      Animal animal = await _db.Animals.FindAsync(id);
+      if (animal == null)
+      {
+        return NotFound();
+      }
+
+      _db.Animals.Remove(animal);
+      await _db.SaveChangesAsync();
+
+      return NoContent();
+    }
+
+    private bool AnimalExists(int id)
+    {
+      return _db.Animals.Any(e => e.AnimalId == id);
+    }
+
   }
 }
+
