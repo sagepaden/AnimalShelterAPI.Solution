cd2a6c9df2a4ee7debca5d446be50a6b9a347d33
sagepaden <sagepaden@gmail.com>
2023-08-25 12:10:38 -0700

    building out initial models and context, pre-seed data

---
+++ ASApi/Models/ASApiContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ASApi.Models
+{
+  public class ASApiContext : DbContext
+  {
+    public DbSet<Animal> Animals { get; set; }
+    public ASApiContext(DbContextOptions<ASApiContext> options) : base(options) { }
+    }
+  }
